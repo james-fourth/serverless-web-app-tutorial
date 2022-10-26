8fa01a1e3efecc6d9ec4677f2e8f44e09d8efe11
James Joseph Potts IV <hello@jamespotts.io>
2022-10-26 12:16:25 -0600

    add user pool config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+	cognito: {
+		userPoolId: 'us-east-1_Ra2Vc4JcR', // e.g. us-east-2_uXboG5pAb
+		userPoolClientId: '6i54gpi140vq53hsapslvl8822', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+		region: 'us-east-1', // e.g. us-east-2
+	},
+	api: {
+		invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+	},
 };
